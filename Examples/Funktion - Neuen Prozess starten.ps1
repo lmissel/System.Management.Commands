bfabd4d92892bc34144f793a7f2bfafba79d2514
lmissel <39304706+lmissel@users.noreply.github.com>
2019-08-12 12:07:20 +0200

    Update Funktion - Neuen Prozess starten.ps1

--- Examples/Funktion - Neuen Prozess starten.ps1
+++ Examples/Funktion - Neuen Prozess starten.ps1
@@ -1,7 +1,15 @@
 ﻿# ----------------------
 # Beispiel - Prozess starten
 # ----------------------
+[System.Management.InvokeMethodOptions] $methodOptions = [System.Management.InvokeMethodOptions]::new($null, [System.TimeSpan]::MaxValue)
 [System.Management.ManagementClass] $processClass = [System.Management.ManagementClass]::new("Win32_Process")
 [System.Management.ManagementBaseObject] $inParams = $processClass.GetMethodParameters("Create")
-$inParams["CommandLine"] = "notepad.exe"
-$processClass.InvokeMethod("Create", $inParams, $null)
+$inParams["CommandLine"] = "C:\\Windows\\System32\\notepad.exe"
+$inParams["CurrentDirectory"] = "C:\\Windows\\System32\\"
+$processClass.InvokeMethod("Create", $inParams, $methodOptions)
+
+# Variante 2
+
+[System.Management.ManagementClass] $processClass = [System.Management.ManagementClass]::new("Win32_Process")
+[Object[]] $methodArgs = "C:\Windows\System32\notepad.exe", $null, $null, 0
+$processClass.InvokeMethod("Create", $methodArgs)
